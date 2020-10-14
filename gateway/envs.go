5105b718c69f8a1ac49551f3ac85e47181eaa3c8
Surya <git@sdan.cc>
2020-10-14 01:21:42 -0700

    revert usage of env tokens in libs

--- gateway/envs.go
+++ gateway/envs.go
@@ -32,20 +32,20 @@ func (g *Gateway) GetEnvs(ctx context.Context, req *entity.GetEnvsRequest) (*ent
 	return resp.Envs, nil
 }
 
-func (g *Gateway) GetEnvsWithEnvToken(ctx context.Context) (*entity.Envs, error) {
+func (g *Gateway) GetEnvsWithProjectToken(ctx context.Context) (*entity.Envs, error) {
 	gqlReq := gql.NewRequest(`
 	  query {
-			allEnvsForEnvToken
+			allEnvsForProjectToken
 		}
 	`)
 
-	err := g.setEnvToken(ctx, gqlReq)
+	err := g.setProjectToken(ctx, gqlReq)
 	if err != nil {
 		return nil, err
 	}
 
 	var resp struct {
-		Envs *entity.Envs `json:"allEnvsForEnvToken"`
+		Envs *entity.Envs `json:"allEnvsForProjectToken"`
 	}
 	if err := g.gqlClient.Run(ctx, gqlReq, &resp); err != nil {
 		return nil, err
