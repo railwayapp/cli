10bdb0928c56185d894d5b7b488ea68ce5eab7d7
Surya <git@sdan.cc>
2020-10-16 17:15:33 -0700

    fix

--- cmd/version.go
+++ cmd/version.go
@@ -14,12 +14,12 @@ func (h *Handler) Version(ctx context.Context, req *entity.CommandRequest) error
 }
 
 func (h *Handler) CheckVersion(ctx context.Context, req *entity.CommandRequest) error {
-	if constants.Version != constants.VersionDefault {
+	if constants.Version != "test" {
 		latest, _ := h.ctrl.GetLatestVersion()
-		latest = latest[1:]
+		latestNoV = latest[1:]
 		// Surpressing error as getting latest version is desired, not required
 
-		if latest != "" && latest != constants.Version {
+		if latest != "" && (latest != constants.Version || latestNoV != constants.Version) {
 			fmt.Printf("A newer version of the Railway CLI is available, please update to: %s\n", latest)
 		}
 	}
