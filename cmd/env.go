23a2e823255e1c8132471b33e421504f470ef083
Surya <git@sdan.cc>
2020-10-19 18:19:14 -0700

    regular envs

--- cmd/env.go
+++ cmd/env.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/railwayapp/cli/entity"
-	"github.com/railwayapp/cli/ui"
 )
 
 func (h *Handler) Env(ctx context.Context, req *entity.CommandRequest) error {
@@ -15,7 +14,7 @@ func (h *Handler) Env(ctx context.Context, req *entity.CommandRequest) error {
 	}
 
 	for k, v := range *envs {
-		fmt.Print(ui.MagentaText(fmt.Sprintf("%-15s%-15s\n", k, v)))
+		fmt.Printf("%-15s%-15s\n", k, v)
 	}
 	return nil
 }
