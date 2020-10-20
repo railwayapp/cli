199203354ed079c3592589b06508b08d0c4aaa33
Jake Runzer <jakerunzer@gmail.com>
2020-10-20 15:06:31 -0600

    Browserless Login (#33)

--- cmd/login.go
+++ cmd/login.go
@@ -9,10 +9,17 @@ import (
 )
 
 func (h *Handler) Login(ctx context.Context, req *entity.CommandRequest) error {
-	user, err := h.ctrl.Login(ctx)
+	isBrowserless, err := req.Cmd.Flags().GetBool("browserless")
 	if err != nil {
 		return err
 	}
-	ui.StopSpinner(fmt.Sprintf("🎉 Logged in as %s (%s)", ui.Bold(user.Name), user.Email))
+
+	user, err := h.ctrl.Login(ctx, isBrowserless)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(fmt.Sprintf("🎉 Logged in as %s (%s)", ui.Bold(user.Name), user.Email))
+
 	return nil
 }
