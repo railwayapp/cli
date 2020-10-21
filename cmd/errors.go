b701470f8bb2f7f1ffa4a7aece72ec75fdb68ea7
Surya <git@sdan.cc>
2020-10-20 17:51:00 -0700

    place errors in contexualize itself

--- cmd/errors.go
+++ cmd/errors.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 )
 
 func (h *Handler) Errors(ctx context.Context, err error) error {
 	// pass along error to controller that passes it to gateway to send it off to backboard
-	h.ctrl.SendError(ctx, err)
+	fmt.Println("hey")
 	//surpress errors
 	return nil
 }
--- gateway/telemetry.go
+++ gateway/telemetry.go
@@ -1,24 +1,12 @@
-package controller
+package gateway
 
 import (
 	"context"
-
-	gql "github.com/machinebox/graphql"
-	"github.com/railwayapp/cli/errors"
+	"fmt"
 )
 
 // GetProject returns a project of id projectId, error otherwise
 func (g *Gateway) SendErrors(ctx context.Context, err error) error {
-	gqlReq := gql.NewRequest(`
-		mutation($projectId: String) {
-			insert error here
-		}
-	`)
-
-	g.authorize(ctx, gqlReq.Header)
-
-	if err := g.gqlClient.Run(ctx, gqlReq); err != nil {
-		return nil, errors.TelemetryFailed
-	}
+	fmt.Println("hey")
 	return nil
 }
