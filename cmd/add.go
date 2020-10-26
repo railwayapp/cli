580d0dd1f9b4bd0d899b66d751fdd1a98143f0bb
Surya <git@sdan.cc>
2020-10-26 13:33:41 -0700

    move into add

--- cmd/add.go
+++ cmd/add.go
@@ -10,18 +10,6 @@ import (
 )
 
 func (h *Handler) addNew(ctx context.Context, req *entity.CommandRequest, pluginsAvailable []*entity.Plugin) (string, error) {
-	pluginRequest, err := h.addNew(ctx, req)
-
-	pluginResp, err := h.ctrl.CreatePlugin(ctx, &entity.CreatePluginRequest{
-		ProjectID: project.Id,
-		Plugin:    pluginRequest,
-	})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("🎉 Created plugin %s\n", ui.MagentaText(pluginResp.Name))
-	return nil
-	//fetch available plugins via controller
 	fmt.Println("Plugins you can create:")
 	pluginSelected, err := ui.PromptPlugins(pluginsAvailable)
 	if err != nil {
@@ -30,17 +18,6 @@ func (h *Handler) addNew(ctx context.Context, req *entity.CommandRequest, plugin
 	return pluginSelected.Name, nil
 }
 func (h *Handler) addExists(ctx context.Context, req *entity.CommandRequest, pluginsAvailable []*entity.Plugin) (string, error) {
-	pluginRequest, err := h.addNew(ctx, req)
-
-	pluginResp, err := h.ctrl.CreatePlugin(ctx, &entity.CreatePluginRequest{
-		ProjectID: project.Id,
-		Plugin:    pluginRequest,
-	})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("🎉 Created plugin %s\n", ui.MagentaText(pluginResp.Name))
-	return nil
 	fmt.Println("You already created that plugin!\nPlugins you can create:")
 	pluginSelected, err := ui.PromptPlugins(pluginsAvailable)
 	if err != nil {
@@ -59,15 +36,16 @@ func (h *Handler) Add(ctx context.Context, req *entity.CommandRequest) error {
 		return err
 	}
 	if len(req.Args[0]) == 0 {
-
-	}
-	pluginRequest := strings.TrimSpace(req.Args[0])
-	allowCreation, pluginsAvailable, err := h.ctrl.PluginExists(ctx, pluginRequest, project.Id)
-	if err != nil {
-		return err
-	}
-	if !allowCreation {
-		pluginRequest, err = h.addExists(ctx, req, pluginsAvailable)
+		pluginRequest, err := h.addNew(ctx, req, h.ctrl.AvailablePlugins(""))
+	} else {
+		pluginRequest := strings.TrimSpace(req.Args[0])
+		allowCreation, pluginsAvailable, err := h.ctrl.PluginExists(ctx, pluginRequest, project.Id)
+		if err != nil {
+			return err
+		}
+		if !allowCreation {
+			pluginRequest, err = h.addExists(ctx, req, pluginsAvailable)
+		}
 	}
 
 	pluginResp, err := h.ctrl.CreatePlugin(ctx, &entity.CreatePluginRequest{
