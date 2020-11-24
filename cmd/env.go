c74cb804b2eec57a49754b09a964497ea2671266
Jake Runzer <jakerunzer@gmail.com>
2020-11-24 11:53:28 -0700

    Use width of longest env var name when printing (#56)

--- cmd/env.go
+++ cmd/env.go
@@ -7,14 +7,38 @@ import (
 	"github.com/railwayapp/cli/entity"
 )
 
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func (h *Handler) Env(ctx context.Context, req *entity.CommandRequest) error {
 	envs, err := h.ctrl.GetEnvs(ctx)
 	if err != nil {
 		return err
 	}
 
+	var minSpacing = 15
+	var maxSpacing = 100
+	var longest = 0
+
+	for k := range *envs {
+		if len(k) > longest {
+			longest = len(k)
+		}
+	}
+
 	for k, v := range *envs {
-		fmt.Printf("%-15s%-15s\n", k, v)
+		fmt.Printf("%-*s\t%s\n", Max(minSpacing, Min(maxSpacing, longest)), k, v)
 	}
 	return nil
 }
