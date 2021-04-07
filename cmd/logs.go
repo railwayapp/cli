eb0d866901ffd02eae62714ed8d911cf5de12fc7
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-06 23:40:14 -0700

    Remove loggin code

--- cmd/logs.go
+++ cmd/logs.go
@@ -7,18 +7,11 @@ import (
 	"time"
 
 	"github.com/railwayapp/cli/entity"
-	"github.com/railwayapp/cli/ui"
 )
 
 func (h *Handler) Logs(ctx context.Context, req *entity.CommandRequest) error {
-	isLoggedIn, _ := h.ctrl.IsLoggedIn(ctx)
-
-	if !isLoggedIn {
-		return fmt.Errorf("%s\nRun %s", ui.RedText("Account require to init project"), ui.Bold("railway login"))
-	}
 
 	prevIdx := 0
-
 	for {
 		err := func() error {
 			defer time.Sleep(2 * time.Second)
