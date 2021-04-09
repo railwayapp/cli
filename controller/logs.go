0a0f7329e486115863a51be1afefb23f18abe6a5
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-09 13:55:31 -0700

    Big refactory

--- controller/logs.go
+++ controller/logs.go
@@ -24,7 +24,7 @@ func (c *Controller) GetActiveDeploymentLogs(ctx context.Context, numLines int32
 		return err
 	}
 
-	return c.LogsForDeployment(ctx, &entity.DeploymentLogsRequest{
+	return c.LogsForState(ctx, &entity.DeploymentLogsRequest{
 		DeploymentID: deployment.ID,
 		ProjectID:    projectID,
 		NumLines:     numLines,
@@ -45,32 +45,27 @@ func (c *Controller) GetActiveBuildLogs(ctx context.Context, numLines int32) err
 		return err
 	}
 
-	return c.LogsForBuild(ctx, &entity.DeploymentLogsRequest{
+	return c.LogsForState(ctx, &entity.DeploymentLogsRequest{
 		DeploymentID: deployment.ID,
 		ProjectID:    projectID,
 		NumLines:     numLines,
 	})
 }
 
-func (c *Controller) LogsForDeployment(ctx context.Context, req *entity.DeploymentLogsRequest) error {
-	// Fetch Initial Deployment Logs
-	query := entity.DeploymentGQL{
-		DeployLogs: true,
-	}
+func (c *Controller) LogsForState(ctx context.Context, req *entity.DeploymentLogsRequest) error {
+	// Stream on building -> Building until !Building then break
+	// Stream on not building -> !Building until Failed then break
 	deploy, err := c.gtwy.GetDeploymentByID(ctx, &entity.DeploymentByIDRequest{
 		DeploymentID: req.DeploymentID,
 		ProjectID:    req.ProjectID,
-		GQL:          query,
+		GQL:          c.getQuery(ctx, ""),
 	})
 	if err != nil {
 		return err
 	}
-	if deploy.Status == entity.STATUS_FAILED {
-		fmt.Println("Build Failed! Please see build logs above for errors")
-		return nil
-	}
-	// Break them down line by line
-	logLines := strings.Split(deploy.DeployLogs, "\n")
+
+	// Print Logs w/ Limit
+	logLines := strings.Split(logsForState(ctx, deploy.Status, deploy), "\n")
 	offset := 0.0
 	if req.NumLines != 0 {
 		// If a limit is set, walk it back n steps (with a min of zero so no panics)
@@ -78,87 +73,66 @@ func (c *Controller) LogsForDeployment(ctx context.Context, req *entity.Deployme
 	}
 	// Output Initial Logs
 	fmt.Println(strings.Join(logLines[int(offset):], "\n"))
-	if req.NumLines == 0 {
-		// If no log limit is set, we stream logs
-		prevLogs := strings.Split(deploy.DeployLogs, "\n")
-		for {
-			time.Sleep(time.Second * 2)
-			deploy, err := c.gtwy.GetDeploymentByID(ctx, &entity.DeploymentByIDRequest{
-				DeploymentID: req.DeploymentID,
-				ProjectID:    req.ProjectID,
-				GQL:          query,
-			})
-			if err != nil {
-				return err
-			}
-			// Current Logs fetched from server
-			currLogs := strings.Split(deploy.DeployLogs, "\n")
-			// Diff logs using the line numbers as references
-			logDiff := currLogs[len(prevLogs)-1 : len(currLogs)-1]
-			// If no changes we continue
-			if len(logDiff) == 0 {
-				continue
-			}
-			// Output logs
-			fmt.Println(strings.Join(logDiff, "\n"))
-			// Set out walk pointer forward using the newest logs
-			prevLogs = currLogs
+
+	prevDeploy := deploy
+	logState := deploy.Status
+	deltaState := hasTransitioned(nil, deploy)
+
+	for !deltaState && req.NumLines == 0 {
+		time.Sleep(2 * time.Second)
+		currDeploy, err := c.gtwy.GetDeploymentByID(ctx, &entity.DeploymentByIDRequest{
+			DeploymentID: req.DeploymentID,
+			ProjectID:    req.ProjectID,
+			GQL:          c.getQuery(ctx, prevDeploy.Status),
+		})
+		if err != nil {
+			return err
 		}
+		// Current Logs fetched from server
+		currLogs := strings.Split(logsForState(ctx, logState, currDeploy), "\n")
+		// Diff logs using the line numbers as references
+		logDiff := currLogs[len(logsForState(ctx, logState, prevDeploy))-1 : len(currLogs)-1]
+		// If no changes we continue
+		if len(logDiff) == 0 {
+			continue
+		}
+		// Output logs
+		fmt.Println(strings.Join(logDiff, "\n"))
+		// Set out walk pointer forward using the newest logs
+		deltaState = hasTransitioned(prevDeploy, currDeploy)
+		prevDeploy = currDeploy
 	}
 	return nil
 }
 
-func (c *Controller) LogsForBuild(ctx context.Context, req *entity.DeploymentLogsRequest) error {
-	// Fetch Initial Deployment Logs
-	query := entity.DeploymentGQL{
-		BuildLogs: true,
-	}
-	deploy, err := c.gtwy.GetDeploymentByID(ctx, &entity.DeploymentByIDRequest{
-		DeploymentID: req.DeploymentID,
-		ProjectID:    req.ProjectID,
-		GQL:          query,
-	})
-	if err != nil {
-		return err
-	}
-	// Break them down line by line
-	logLines := strings.Split(deploy.BuildLogs, "\n")
-	offset := 0.0
-	if req.NumLines != 0 {
-		// If a limit is set, walk it back n steps (with a min of zero so no panics)
-		offset = math.Max(float64(len(logLines))-float64(req.NumLines)-1, 0.0)
-	}
-	// Output Initial Logs
-	fmt.Println(strings.Join(logLines[int(offset):], "\n"))
-	if req.NumLines == 0 {
-		// If no log limit is set, we stream logs
-		prevLogs := strings.Split(deploy.BuildLogs, "\n")
-		for {
-			time.Sleep(time.Second * 2)
-			deploy, err := c.gtwy.GetDeploymentByID(ctx, &entity.DeploymentByIDRequest{
-				DeploymentID: req.DeploymentID,
-				ProjectID:    req.ProjectID,
-				GQL:          query,
-			})
-			if err != nil {
-				return err
-			}
-			// Current Logs fetched from server
-			currLogs := strings.Split(deploy.BuildLogs, "\n")
-			// Diff logs using the line numbers as references
-			logDiff := currLogs[len(prevLogs)-1 : len(currLogs)-1]
-			// If no changes we continue
-			if len(logDiff) == 0 {
-				continue
-			}
-			// Output logs
-			fmt.Println(strings.Join(logDiff, "\n"))
-			// Set out walk pointer forward using the newest logs
-			prevLogs = currLogs
-			if deploy.Status != entity.STATUS_BUILDING {
-				break
-			}
+func hasSoftError(logs []string) bool {
+	for _, s := range logs {
+		if s == "Error fetching build logs" {
+			return true
 		}
 	}
-	return nil
+	return false
+}
+
+func hasTransitioned(prev *entity.Deployment, curr *entity.Deployment) bool {
+	return prev != nil && curr != nil && prev.Status != curr.Status
+}
+
+func isBuilding(ctx context.Context, status string) bool {
+	return status == entity.STATUS_BUILDING
+}
+
+func (c *Controller) getQuery(ctx context.Context, status string) entity.DeploymentGQL {
+	return entity.DeploymentGQL{
+		BuildLogs:  status == entity.STATUS_BUILDING || status == "",
+		DeployLogs: status != entity.STATUS_BUILDING || status == "",
+		Status:     true,
+	}
+}
+
+func logsForState(ctx context.Context, state string, deploy *entity.Deployment) string {
+	if isBuilding(ctx, state) {
+		return deploy.BuildLogs
+	}
+	return deploy.DeployLogs
 }
