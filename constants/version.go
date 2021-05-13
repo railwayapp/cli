c255a1dff03e77fc2e47fe9e212b6db3935e6835
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-05-12 17:43:47 -0700

    Move to URL constant file

--- constants/version.go
+++ constants/version.go
@@ -3,7 +3,3 @@ package constants
 const VersionDefault = "Piped into LDflags on build. You are probably running Railway CLI from source."
 
 var Version string = VersionDefault
-
-const RailwayURLDefault = "https://railway.app"
-
-var RAILWAY_URL string = RailwayURLDefault
