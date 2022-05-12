97510863831939036889a1369cab943d34000363
wyzlle <101371689+wyzlle@users.noreply.github.com>
2022-05-12 21:41:15 +0000

    Add support for android

--- npm-install/config.js
+++ npm-install/config.js
@@ -33,8 +33,7 @@ export const CONFIG = {
 export const ARCH_MAPPING = {
   ia32: "386",
   x64: "amd64",
-  // Re-add when railwayapp/cli#173 gets merged
-  // arm: "arm",
+  arm: "arm",
   arm64: "arm64",
 };
 
@@ -45,5 +44,6 @@ export const PLATFORM_MAPPING = {
   darwin: "darwin",
   linux: "linux",
   win32: "windows",
+  android: "android",
   // freebsd: "freebsd",
 };
