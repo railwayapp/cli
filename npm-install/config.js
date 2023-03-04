298c2c66c3db4626e3790db2731af175bcf4bda1
nebulatgs <nebula@railway.app>
2023-03-04 00:41:31 -0500

    remove commented code

--- npm-install/config.js
+++ npm-install/config.js
@@ -26,22 +26,3 @@ export const CONFIG = {
 	 */
 	url: "https://github.com/railwayapp/cli/releases/download/v{{version}}/{{bin_name}}-v{{version}}-{{triple}}.tar.gz",
 };
-
-// /**
-//  * Mapping from Node's `process.arch` to Golang's `$GOARCH`
-//  */
-// export const ARCH_MAPPING = {
-// 	ia32: "386",
-// 	x64: "amd64",
-// 	arm64: "arm64",
-// };
-
-// /**
-//  * Mapping between Node's `process.platform` to Golang's
-//  */
-// export const PLATFORM_MAPPING = {
-// 	darwin: "darwin",
-// 	linux: "linux",
-// 	win32: "windows",
-// 	// freebsd: "freebsd",
-// };
