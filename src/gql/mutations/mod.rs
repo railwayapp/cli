d860d697eb4281235f79c327672a3d4d49a202e6
Pavel Fomchenkov <hello@pavel.codes>
2023-03-07 01:55:32 +0100

    Reorder the structs of mutations alphabetically

--- src/gql/mutations/mod.rs
+++ src/gql/mutations/mod.rs
@@ -3,6 +3,22 @@ use graphql_client::GraphQLQuery;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/gql/schema.graphql",
+    query_path = "src/gql/mutations/strings/LoginSessionConsume.graphql",
+    response_derives = "Debug, Serialize, Clone"
+)]
+pub struct LoginSessionConsume;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/gql/schema.graphql",
+    query_path = "src/gql/mutations/strings/LoginSessionCreate.graphql",
+    response_derives = "Debug, Serialize, Clone"
+)]
+pub struct LoginSessionCreate;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/gql/schema.graphql",
     query_path = "src/gql/mutations/strings/PluginCreate.graphql",
     response_derives = "Debug, Serialize, Clone"
 )]
@@ -19,14 +35,6 @@ pub struct PluginDelete;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/gql/schema.graphql",
-    query_path = "src/gql/mutations/strings/ValidateTwoFactor.graphql",
-    response_derives = "Debug, Serialize, Clone"
-)]
-pub struct ValidateTwoFactor;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/gql/schema.graphql",
     query_path = "src/gql/mutations/strings/ProjectCreate.graphql",
     response_derives = "Debug, Serialize, Clone"
 )]
@@ -35,23 +43,15 @@ pub struct ProjectCreate;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/gql/schema.graphql",
-    query_path = "src/gql/mutations/strings/LoginSessionCreate.graphql",
-    response_derives = "Debug, Serialize, Clone"
-)]
-pub struct LoginSessionCreate;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/gql/schema.graphql",
-    query_path = "src/gql/mutations/strings/LoginSessionConsume.graphql",
-    response_derives = "Debug, Serialize, Clone"
-)]
-pub struct LoginSessionConsume;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/gql/schema.graphql",
     query_path = "src/gql/mutations/strings/ServiceDomainCreate.graphql",
     response_derives = "Debug, Serialize, Clone"
 )]
 pub struct ServiceDomainCreate;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/gql/schema.graphql",
+    query_path = "src/gql/mutations/strings/ValidateTwoFactor.graphql",
+    response_derives = "Debug, Serialize, Clone"
+)]
+pub struct ValidateTwoFactor;
