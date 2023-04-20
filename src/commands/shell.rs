864e08ad1e4f6fb6b9de694abc6e92c49b71d26a
Alexander Ng <alexng353@gmail.com>
2023-04-20 14:43:14 -0700

    feat: windows specific shell detection

--- src/commands/shell.rs
+++ src/commands/shell.rs
@@ -73,20 +73,98 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
         eprintln!("No service linked, skipping service variables");
     }
 
+    enum WindowsShell {
+        Cmd,
+        Powershell,
+        Powershell7,
+    }
+
+    async fn windows_shell_detection() -> Option<WindowsShell> {
+        let pid = std::process::id().to_string();
+
+        // https://stackoverflow.com/questions/7486717/finding-parent-process-id-on-windows
+        // runs the command "wmic process get processid,parentprocessid,executablepath|find "process id goes here"
+        // this is to determine the parent process of the current process, which should give the shell.
+        // Only matches cmd, powershell or pwsh for safety
+        let wmic = tokio::process::Command::new("wmic")
+            .args(&["process", "get", "executablepath,processid,parentprocessid"])
+            .output()
+            .await
+            .context("Failed to run wmic command")
+            .unwrap();
+        let wmic = String::from_utf8(wmic.stdout)
+            .context("Failed to convert wmic output to utf8")
+            .unwrap();
+
+        let current_process = wmic
+            .lines()
+            .find(|line| line.contains(&pid))
+            .context("Failed to find current process in wmic output")
+            .unwrap()
+            .to_string();
+
+        dbg!(&current_process);
+
+        let pids = current_process.split_whitespace().collect::<Vec<&str>>();
+        let pids = pids
+            .iter()
+            .filter(|pid| pid.parse::<u32>().is_ok())
+            .collect::<Vec<&&str>>();
+
+        let parent_pid = pids[0].to_string();
+
+        let parent_output_line = wmic
+            .lines()
+            .find(|line| line.contains(&parent_pid))
+            .context("Failed to find parent process in wmic output")
+            .unwrap()
+            .to_string();
+
+        let parent_executable = parent_output_line
+            .split("exe")
+            .next()
+            .context("Failed to find parent executable")
+            .unwrap()
+            .to_string()
+            + "exe";
+
+        if parent_executable.contains("pwsh") {
+            Some(WindowsShell::Powershell7)
+        } else if parent_executable.contains("powershell") {
+            Some(WindowsShell::Powershell)
+        } else {
+            Some(WindowsShell::Cmd)
+        }
+    }
+
     let shell = std::env::var("SHELL").unwrap_or(match std::env::consts::OS {
-        "windows" => "cmd".to_string(),
+        "windows" => match windows_shell_detection().await {
+            Some(WindowsShell::Powershell) => "powershell".to_string(),
+            Some(WindowsShell::Cmd) => "cmd".to_string(),
+            Some(WindowsShell::Powershell7) => "pwsh".to_string(),
+            None => "cmd".to_string(),
+        },
         _ => "sh".to_string(),
     });
 
-    println!("Entering subshell with Railway variables available. Type 'exit' to exit.");
+    let shell_options = match shell.as_str() {
+        "powershell" => vec!["/nologo"],
+        "pwsh" => vec!["/nologo"],
+        "cmd" => vec!["/k"],
+        _ => vec![],
+    };
+
+    println!("Entering subshell with Railway variables available. Type 'exit' to exit.\n");
 
     tokio::process::Command::new(shell)
+        .args(shell_options)
         .envs(all_variables)
         .spawn()
         .context("Failed to spawn command")?
         .wait()
         .await
         .context("Failed to wait for command")?;
+
     println!("Exited subshell, Railway variables no longer available.");
     Ok(())
 }
