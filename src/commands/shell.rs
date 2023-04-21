b4d734b281df0a519e08c4e1fa1af0846d9108db
Alexander Ng <alexng353@gmail.com>
2023-04-20 18:58:07 -0700

    implementation using winapi

--- src/commands/shell.rs
+++ src/commands/shell.rs
@@ -4,6 +4,19 @@ use crate::consts::SERVICE_NOT_FOUND;
 
 use super::*;
 
+use serde_json::from_str;
+use winapi::shared::minwindef::DWORD;
+use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
+use winapi::um::processthreadsapi::GetCurrentProcessId;
+use winapi::um::tlhelp32::{
+    CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
+};
+
+extern crate winapi;
+
+use std::ffi::CStr;
+use std::mem::zeroed;
+
 /// Open a subshell with Railway variables available
 #[derive(Parser)]
 pub struct Args {
@@ -79,56 +92,17 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
         Powershell7,
     }
 
+    /// https://gist.github.com/mattn/253013/d47b90159cf8ffa4d92448614b748aa1d235ebe4
+    /// Only matches cmd, powershell or pwsh for safety
+    /// defaults to cmd if no parent process is found
     async fn windows_shell_detection() -> Option<WindowsShell> {
-        let pid = std::process::id().to_string();
-
-        // https://stackoverflow.com/questions/7486717/finding-parent-process-id-on-windows
-        // runs the command "wmic process get processid,parentprocessid,executablepath|find "process id goes here"
-        // this is to determine the parent process of the current process, which should give the shell.
-        // Only matches cmd, powershell or pwsh for safety
-        let wmic = tokio::process::Command::new("wmic")
-            .args(&["process", "get", "executablepath,processid,parentprocessid"])
-            .output()
-            .await
-            .context("Failed to run wmic command")
-            .unwrap();
-        let wmic = String::from_utf8(wmic.stdout)
-            .context("Failed to convert wmic output to utf8")
+        let (_, ppname) = get_parent_process_info()
+            .context("Failed to get parent process info")
             .unwrap();
 
-        let current_process = wmic
-            .lines()
-            .find(|line| line.contains(&pid))
-            .context("Failed to find current process in wmic output")
-            .unwrap()
-            .to_string();
-
-        let pids = current_process.split_whitespace().collect::<Vec<&str>>();
-        let pids = pids
-            .iter()
-            .filter(|pid| pid.parse::<u32>().is_ok())
-            .collect::<Vec<&&str>>();
-
-        let parent_pid = pids[0].to_string();
-
-        let parent_output_line = wmic
-            .lines()
-            .find(|line| line.contains(&parent_pid))
-            .context("Failed to find parent process in wmic output")
-            .unwrap()
-            .to_string();
-
-        let parent_executable = parent_output_line
-            .split("exe")
-            .next()
-            .context("Failed to find parent executable")
-            .unwrap()
-            .to_string()
-            + "exe";
-
-        if parent_executable.contains("pwsh") {
+        if ppname.contains("pwsh") {
             Some(WindowsShell::Powershell7)
-        } else if parent_executable.contains("powershell") {
+        } else if ppname.contains("powershell") {
             Some(WindowsShell::Powershell)
         } else {
             Some(WindowsShell::Cmd)
@@ -165,4 +139,71 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
 
     println!("Exited subshell, Railway variables no longer available.");
     Ok(())
+}
+
+/// get the parent process info, translated from
+// https://gist.github.com/mattn/253013/d47b90159cf8ffa4d92448614b748aa1d235ebe4
+fn get_parent_process_info() -> Option<(DWORD, String)> {
+    let mut pe32: PROCESSENTRY32 = unsafe { zeroed() };
+    let pid = unsafe { GetCurrentProcessId() };
+    let h_snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+    let mut ppid = 0;
+
+    if h_snapshot == INVALID_HANDLE_VALUE {
+        return None;
+    }
+
+    pe32.dwSize = std::mem::size_of::<PROCESSENTRY32>() as u32;
+
+    if unsafe { Process32First(h_snapshot, &mut pe32) } != 0 {
+        loop {
+            if pe32.th32ProcessID == pid {
+                ppid = pe32.th32ParentProcessID;
+                break;
+            }
+            if unsafe { Process32Next(h_snapshot, &mut pe32) } == 0 {
+                break;
+            }
+        }
+    }
+
+    let mut parent_process_name = None;
+    if ppid != 0 {
+        parent_process_name = get_process_name(ppid);
+    }
+
+    unsafe { CloseHandle(h_snapshot) };
+
+    if let Some(ppname) = parent_process_name {
+        Some((ppid, ppname))
+    } else {
+        None
+    }
+}
+fn get_process_name(pid: DWORD) -> Option<String> {
+    let mut pe32: PROCESSENTRY32 = unsafe { zeroed() };
+    let h_snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+
+    if h_snapshot == INVALID_HANDLE_VALUE {
+        return None;
+    }
+
+    pe32.dwSize = std::mem::size_of::<PROCESSENTRY32>() as u32;
+
+    if unsafe { Process32First(h_snapshot, &mut pe32) } != 0 {
+        loop {
+            if pe32.th32ProcessID == pid {
+                let process_name_cstr = unsafe { CStr::from_ptr(pe32.szExeFile.as_ptr()) };
+                let process_name = process_name_cstr.to_string_lossy().into_owned();
+                unsafe { CloseHandle(h_snapshot) };
+                return Some(process_name);
+            }
+            if unsafe { Process32Next(h_snapshot, &mut pe32) } == 0 {
+                break;
+            }
+        }
+    }
+
+    unsafe { CloseHandle(h_snapshot) };
+    None
 }
