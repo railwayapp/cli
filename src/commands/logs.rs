74ad35b5158dd97b4abbf26b6da93b5b7fb21201
Milo <50248166+Milo123459@users.noreply.github.com>
2023-09-20 17:40:42 +0000

    attempt to get a service and environment

--- src/commands/logs.rs
+++ src/commands/logs.rs
@@ -1,10 +1,23 @@
-use crate::controllers::deployment::{stream_build_logs, stream_deploy_logs};
+use crate::controllers::{
+    deployment::{stream_build_logs, stream_deploy_logs},
+    environment::get_matched_environment,
+    project::get_project,
+};
+use anyhow::bail;
 
 use super::{queries::deployments::DeploymentStatus, *};
 
 /// View the most-recent deploy's logs
 #[derive(Parser)]
 pub struct Args {
+    /// Service to view logs from (defaults to linked service)
+    #[clap(short, long)]
+    service: Option<String>,
+
+    /// Environment to view logs from (defaults to linked environment)
+    #[clap(short, long)]
+    environment: Option<String>,
+
     /// Show deployment logs
     #[clap(short, long, group = "log_type")]
     deployment: bool,
@@ -18,6 +31,33 @@ pub async fn command(args: Args, json: bool) -> Result<()> {
     let configs = Configs::new()?;
     let client = GQLClient::new_authorized(&configs)?;
     let linked_project = configs.get_linked_project().await?;
+
+    let project = get_project(&client, &configs, linked_project.project.clone()).await?;
+
+    let environment = args
+        .environment
+        .clone()
+        .unwrap_or(linked_project.environment.clone());
+
+    let services = project.services.edges.iter().collect::<Vec<_>>();
+
+    let environment_id = get_matched_environment(&project, environment)?.id;
+    let service = if let Some(service_arg) = args.service {
+        // If the user specified a service, use that
+        let service_id = services
+            .iter()
+            .find(|service| service.node.name == service_arg || service.node.id == service_arg);
+        if let Some(service_id) = service_id {
+            Some(service_id.node.id.to_owned())
+        } else {
+            bail!("Service not found");
+        }
+    } else if let Some(service) = linked_project.service {
+        // If the user didn't specify a service, but we have a linked service, use that
+        Some(service)
+    } else {
+        bail!("No service could be found. Please either link one with `railway service` or specify one via the `--service` flag.");
+    }.unwrap();
 
     let vars = queries::deployments::Variables {
         project_id: linked_project.project.clone(),
