4139b94ac7d141bd37e3c95b9eb8ac0e27f2fb2e
Milo <50248166+Milo123459@users.noreply.github.com>
2023-09-20 18:35:29 +0000

    fix down command

--- src/commands/down.rs
+++ src/commands/down.rs
@@ -1,12 +1,25 @@
 use std::time::Duration;
 
-use crate::{consts::TICK_STRING, util::prompt::prompt_confirm_with_default};
+use anyhow::bail;
 
-use super::*;
+use super::{queries::deployments::DeploymentListInput, *};
+use crate::{
+    consts::TICK_STRING,
+    controllers::{environment::get_matched_environment, project::get_project},
+    util::prompt::prompt_confirm_with_default,
+};
 
 /// Remove the most recent deployment
 #[derive(Parser)]
 pub struct Args {
+    /// Service to remove the deployment from (defaults to linked service)
+    #[clap(short, long)]
+    service: Option<String>,
+
+    /// Environment to remove the deployment from (defaults to linked environment)
+    #[clap(short, long)]
+    environment: Option<String>,
+
     /// Skip confirmation dialog
     #[clap(short = 'y', long = "yes")]
     bypass: bool,
@@ -16,6 +29,43 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
     let configs = Configs::new()?;
     let client = GQLClient::new_authorized(&configs)?;
     let linked_project = configs.get_linked_project().await?;
+
+    let project = get_project(&client, &configs, linked_project.project.clone()).await?;
+
+    let environment = args
+        .environment
+        .clone()
+        .unwrap_or(linked_project.environment.clone());
+
+    let services = project.services.edges.iter().collect::<Vec<_>>();
+
+    let environment_id = get_matched_environment(&project, environment)?.id;
+    let service = if let Some(service_arg) = args.service {
+        // If the user specified a service, use that
+        let service_id = services
+            .iter()
+            .find(|service| service.node.name == service_arg || service.node.id == service_arg);
+        if let Some(service_id) = service_id {
+            Some(service_id.node.id.to_owned())
+        } else {
+            bail!("Service not found");
+        }
+    } else if let Some(service) = linked_project.service {
+        // If the user didn't specify a service, but we have a linked service, use that
+        Some(service)
+    } else {
+        bail!("No service could be found. Please either link one with `railway service` or specify one via the `--service` flag.");
+    }.unwrap();
+
+    let vars = queries::deployments::Variables {
+        input: DeploymentListInput {
+            project_id: Some(linked_project.project.clone()),
+            environment_id: Some(environment_id),
+            service_id: Some(service),
+            include_deleted: None,
+            status: None,
+        },
+    };
 
     let linked_project_name = linked_project
         .name
@@ -31,14 +81,9 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
         linked_project_name.bold()
     );
 
-    let vars = queries::deployments::Variables {
-        project_id: linked_project.project.clone(),
-    };
-
     let deployments =
         post_graphql::<queries::Deployments, _>(&client, configs.get_backboard(), vars)
             .await?
-            .project
             .deployments;
 
     let mut deployments: Vec<_> = deployments
