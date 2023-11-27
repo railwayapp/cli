95332b9f54487edea0d1c3b8c962d317ce932307
Milo <50248166+Milo123459@users.noreply.github.com>
2023-11-27 10:29:29 +0000

    new way of formamt

--- src/util/logs.rs
+++ src/util/logs.rs
@@ -1,56 +1,46 @@
 use crate::subscriptions;
 use colored::Colorize;
 
-pub fn format_attr_log(mut log: subscriptions::deployment_logs::LogFields) {
+pub fn format_attr_log(log: subscriptions::deployment_logs::LogFields) {
     if !log.attributes.is_empty() {
         let mut level: Option<String> = None;
         let message = log.message;
         let mut others = Vec::new();
-        // for some reason, not all have "" around the value
-        for attr in &mut log.attributes {
-            if !attr.value.starts_with('"') {
-                attr.value.insert(0, '"');
-            };
-            if !attr.value.ends_with('"') {
-                attr.value.push('"');
-            }
-        }
         // get attributes using a match
         for attr in &log.attributes {
             match attr.key.to_lowercase().as_str() {
                 "level" | "lvl" | "severity" => level = Some(attr.value.clone()),
                 _ => others.push(format!(
                     "{}{}{}",
-                    attr.key.clone().bright_cyan(),
+                    attr.key.clone().magenta(),
                     "=",
                     attr.value
                         .clone()
+                        .normal()
                         .replace('"', "\"".dimmed().to_string().as_str())
                 )),
             }
         }
         // get the level and colour it
-        let level = level.map(|level| {
-            // make it uppercase so we dont have to make another variable
-            // for some reason, .uppercase() removes formatting
-            let level = level.replace('"', "").to_uppercase();
-            match level.to_lowercase().as_str() {
-                "info" => "INFO".blue(),
-                "error" | "err" => "ERRO".red(),
-                "warn" => "WARN".yellow(),
-                "debug" => "DBUG".magenta(),
-                _ => level.normal(),
-            }
-            .bold()
-        });
+        let level = level
+            .map(|level| {
+                // make it uppercase so we dont have to make another variable
+                // for some reason, .uppercase() removes formatting
+
+                match level.replace('"', "").to_lowercase().as_str() {
+                    "info" => "[INFO]".blue(),
+                    "error" | "err" => "[ERRO]".red(),
+                    "warn" => "[WARN]".yellow(),
+                    "debug" => "[DBUG]".dimmed(),
+                    _ => format!("[{}]", level).normal(),
+                }
+                .bold()
+            })
+            .unwrap();
         println!(
-            "{}={} {}={} {}={}{}{5} {}",
-            "timestamp".bright_cyan(),
-            log.timestamp.replace('"', "").purple(),
-            "level".bright_cyan(),
-            level.unwrap_or_default(),
-            "msg".bright_cyan(),
-            "\"".dimmed(),
+            "{} {} {} {}",
+            log.timestamp.replace('"', "").normal(),
+            level,
             message,
             others.join(" ")
         );
