06538886c125e7dfe8994c3772d8d84c909f1bca
Milo <50248166+Milo123459@users.noreply.github.com>
2024-02-12 20:11:56 +0000

    properly remove old mutations

--- src/gql/mutations/mod.rs
+++ src/gql/mutations/mod.rs
@@ -27,22 +27,6 @@ pub struct LoginSessionCreate;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/gql/schema.graphql",
-    query_path = "src/gql/mutations/strings/PluginCreate.graphql",
-    response_derives = "Debug, Serialize, Clone"
-)]
-pub struct PluginCreate;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/gql/schema.graphql",
-    query_path = "src/gql/mutations/strings/PluginDelete.graphql",
-    response_derives = "Debug, Serialize, Clone"
-)]
-pub struct PluginDelete;
-
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/gql/schema.graphql",
     query_path = "src/gql/mutations/strings/ProjectCreate.graphql",
     response_derives = "Debug, Serialize, Clone"
 )]
