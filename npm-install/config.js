acc4740e3dd476fd7a10d40eb128e3215c305e93
Milo <50248166+Milo123459@users.noreply.github.com>
2024-05-12 20:53:47 +0100

    tweak npm install 🫤

--- npm-install/config.js
+++ npm-install/config.js
@@ -20,9 +20,8 @@ export const CONFIG = {
 	 * - `{{arch}}` is one of the Golang achitectures listed below
 	 * - `{{bin_name}}` is the name declared above
 	 * - `{{platform}}` is one of the Golang platforms listed below
-	 * - `{{version}}` is the version number as `0.0.0` (taken from package.json)
 	 *
 	 * @type {string}
 	 */
-	url: "https://github.com/railwayapp/cli/releases/download/v{{version}}/{{bin_name}}-v{{version}}-{{triple}}.tar.gz",
+	url: "https://github.com/railwayapp/cli/releases/download/v{{version}}/{{bin_name}}-{{triple}}.tar.gz",
 };
