b121ebd4b719ddbb12417239f6031bccb4d4272c
Milo <50248166+Milo123459@users.noreply.github.com>
2024-06-02 23:13:16 +0100

    list command

--- src/commands/volume.rs
+++ src/commands/volume.rs
@@ -1,7 +1,7 @@
 use super::*;
 use crate::{
     controllers::project::get_project,
-    queries::project::ProjectProject,
+    queries::project::{ProjectProject, ProjectProjectVolumesEdges},
     util::prompt::{fake_select, prompt_text},
 };
 use anyhow::{anyhow, bail};
@@ -58,9 +58,74 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
 
     match args.command {
         Commands::Add(a) => add(service, environment, a.mount_path, project).await?,
+        Commands::List => list(environment, project).await?,
         _ => unimplemented!(),
     }
 
+    Ok(())
+}
+
+async fn list(environment: String, project: ProjectProject) -> Result<()> {
+    let volumes: Vec<&ProjectProjectVolumesEdges> = project
+        .volumes
+        .edges
+        .iter()
+        .filter(|v| {
+            v.node
+                .volume_instances
+                .edges
+                .iter()
+                .any(|a| a.node.environment_id == environment.clone())
+        })
+        .collect();
+    let environment_name = project
+        .environments
+        .edges
+        .iter()
+        .find(|e| e.node.id == environment)
+        .map(|e| e.node.name.clone())
+        .unwrap();
+
+    if volumes.is_empty() {
+        bail!(
+            "No volumes found in environment {}",
+            environment_name.blue()
+        );
+    } else {
+        println!("Project: {}", project.name.cyan().bold());
+        println!("Environment: {}", environment_name.cyan().bold());
+        for volume in volumes {
+            println!();
+            let volume = volume
+                .node
+                .volume_instances
+                .edges
+                .iter()
+                .find(|a| a.node.environment_id == environment.clone())
+                .unwrap();
+            println!("Volume: {}", volume.node.volume.name.green());
+            println!(
+                "Attached to: {}",
+                project
+                    .services
+                    .edges
+                    .iter()
+                    .find(|s| s.node.id == volume.node.service_id.clone().unwrap())
+                    .unwrap()
+                    .node
+                    .name
+                    .purple()
+            );
+            println!("Mount path: {}", volume.node.mount_path.yellow());
+            println!(
+                "Storage used: {}{}/{}{}",
+                volume.node.current_size_mb.round().to_string().blue(),
+                "MB".blue(),
+                volume.node.size_mb.to_string().red(),
+                "MB".red()
+            )
+        }
+    }
     Ok(())
 }
 
