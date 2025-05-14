c5a2280290a5f29102d6092ecb7288f786618a45
Milo <50248166+Milo123459@users.noreply.github.com>
2025-05-14 17:35:14 +0100

    Fix the `--json` flag (#630)
    * fix json
    
    * remove dbg

--- src/macros.rs
+++ src/macros.rs
@@ -16,6 +16,7 @@ macro_rules! commands {
                         clap::Arg::new("json")
                             .long("json")
                             .action(clap::ArgAction::SetTrue)
+                            .global(true)
                             .help("Output in JSON format")
                     );
                 $(
@@ -50,12 +51,13 @@ macro_rules! commands {
 
             /// Dispatches the selected subcommand (after parsing) to its handler.
             pub async fn exec_cli(matches: clap::ArgMatches) -> anyhow::Result<()> {
+                let json = matches.get_flag("json");
                 match matches.subcommand() {
                     $(
                         Some((stringify!([<$module:snake>]), sub_matches)) => {
                                let args = <$module::Args as ::clap::FromArgMatches>::from_arg_matches(sub_matches)
                                    .map_err(anyhow::Error::from)?;
-                            $module::command(args, matches.get_flag("json")).await?;
+                            $module::command(args, json).await?;
                         },
                     )*
                     _ => {
