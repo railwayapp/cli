9eba10fdf94f32f7c734224d84166c126373567e
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2026-03-13 14:52:26 -0400

    fix: update nix shell pins to provide Rust 1.94 for edition2024 support

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/313b84933167.tar.gz") {
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/50ab793786d9.tar.gz") {
     overlays = [
-      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/577ee84c69ba.tar.gz"))
+      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/475826b105eb.tar.gz"))
     ];
   }
 }:
